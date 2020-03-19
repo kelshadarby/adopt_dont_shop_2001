92e5a7198afe2e3e8b045485f826fc0dca25da74
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-18 22:08:50 -0600

    Move show route to bottm

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/shelters', to: 'shelters#index'
-  get '/shelters/:id', to: "shelters#show"
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
+  get '/shelters/:id', to: 'shelters#show'
 end
