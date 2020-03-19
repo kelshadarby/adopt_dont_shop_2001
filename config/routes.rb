8a15466caa83d8f4b036ebb6c5b15402d7067eb3
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-18 19:43:36 -0600

    Add new POST route for the create method

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/shelters', to: 'shelters#index'
   get '/shelters/:id', to: "shelters#show"
   get '/shelters/new', to: 'shelters#new'
+  post '/shelters', to: 'shelters#create'
 end
