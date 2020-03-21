26dad9984e096a0d1b8493444886856b1880d0ea
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-20 22:30:58 -0600

    Create pet model

---
+++ app/models/pet.rb
@@ -0,0 +1,9 @@
+class Pet < ApplicationRecord
+  validates_presence_of :image
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :approximate_age
+  validates_presence_of :sex
+  validates_presence_of :adoption_status
+  belongs_to :shelter
+end
