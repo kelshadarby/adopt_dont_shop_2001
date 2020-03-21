77959d5b52a29a86eb07fb182c28247a45559f07
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-20 22:22:17 -0600

    Edit pets index page test to have all attributes and expectations for those attributes

--- spec/features/pets/user_can_view_pets_index_page_spec.rb
+++ spec/features/pets/user_can_view_pets_index_page_spec.rb
@@ -11,9 +11,11 @@ RSpec.describe "as a user", type: :feature do
 
     pet_1 = Pet.create(image: "https://i.pinimg.com/originals/a6/0f/7f/a60f7f075fcbe60956a31179c5eff08c.jpg",
                        name: "Spyro",
+                       description: "My name is Spyro. I love long walks in the park.. or uhh... anywhere really... and hanging with my pals. I work hard and I play hard, even if I don't work hard. 'Swipe right' if you want an energetic pal to go on hikes with.",
                        approximate_age: 2,
                        sex: "Male",
-                       current_shelter: shelter_1.name)
+                       adoption_status: "available",
+                       shelter_id: shelter_1.id)
 
     visit "/pets"
 
@@ -21,7 +23,9 @@ RSpec.describe "as a user", type: :feature do
     expect(page).to have_content(pet_1.name)
     expect(page).to have_content(pet_1.approximate_age)
     expect(page).to have_content(pet_1.sex)
-    expect(page).to have_content(pet_1.current_shelter)
-    expect(pet_1.current_shelter).to eq(shelter_1.name)
+    expect(page).to have_content(shelter_1.name)
+
+    expect(page).to_not have_content(pet_1.description)
+    expect(page).to_not have_content(pet_1.adoption_status)
   end
 end
