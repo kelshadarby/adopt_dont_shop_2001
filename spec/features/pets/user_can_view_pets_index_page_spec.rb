5c0715b744e369a3b49ee23f1570ce2e7145c3e5
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-20 20:48:38 -0600

    Write pets index page test spec

---
+++ spec/features/pets/user_can_view_pets_index_page_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe "as a user", type: :feature do
+  it "can see all pets in system, including image, name, approximate age, sex, and shelter" do
+
+    shelter_1 = Shelter.create(name: "Adams County Animal Shelter",
+      address: "10705 Fulton St",
+      city: "Brighton",
+      state: "CO",
+      zip: "80601")
+
+    pet_1 = Pet.create(image: "https://i.pinimg.com/originals/a6/0f/7f/a60f7f075fcbe60956a31179c5eff08c.jpg",
+                       name: "Spyro",
+                       approximate_age: 2,
+                       sex: "Male",
+                       current_shelter: shelter_1.name)
+
+    visit "/pets"
+
+    expect(page).to have_content(pet_1.image)
+    expect(page).to have_content(pet_1.name)
+    expect(page).to have_content(pet_1.approximate_age)
+    expect(page).to have_content(pet_1.sex)
+    expect(page).to have_content(pet_1.current_shelter)
+    expect(pet_1.current_shelter).to eq(shelter_1.name)
+  end
+end
+
+# As a visitor
+# When I visit '/pets'
+# Then I see each Pet in the system including the Pet's:
+# - image
+# - name
+# - approximate age
+# - sex
+# - name of the shelter where the pet is currently located
