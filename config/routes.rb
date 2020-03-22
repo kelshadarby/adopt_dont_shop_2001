c8a6ef2b264db688efe9e23cca004596b7a41dd7
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-22 13:42:18 -0600

    Create shelter pets index route

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   delete '/shelters/:id', to: 'shelters#destroy'
 
   get '/pets', to: 'pets#index'
+
+  get '/shelter/:shelter_id/pets', to: 'shelter_pets#index'
 end
