6067ced6adb3bcbd5ef376a88776e9d40a1fce54
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-22 22:17:01 -0600

    Edit drop down test in shelter pet test

--- spec/features/shelter_pets/user_can_create_shelter_pets_spec.rb
+++ spec/features/shelter_pets/user_can_create_shelter_pets_spec.rb
@@ -18,9 +18,9 @@ RSpec.describe "as a user", type: :feature do
     expect(page).to have_field('Name')
     expect(page).to have_field('Description (About Me)')
     expect(page).to have_field('Approximate Age')
-    expect(page).to have_selector('Sex')
+    expect(page).to have_field('Sex')
 
-    fill_in 'Image URL', with: 'https://i.pinimg.com/originals/30/f7/eb/30f7eb5efeda4be553a9daa8cb2e90d8.jpg'
+    fill_in 'Image URL', with: "https://dogsofsf.com/wp-content/uploads/2016/05/IMG_1391.jpg"
     fill_in 'Name', with: 'Rex'
     fill_in 'Description (About Me)', with: 'Hi, I’m Rex. I’m a Basset Hound, I think walks are rather overrated. I prefer to sleep by the fire but I can bribed to get up if there is food. Come hang out by the fire with me.'
     fill_in 'Approximate Age', with: '8 y/o'
@@ -29,15 +29,12 @@ RSpec.describe "as a user", type: :feature do
 
     expect(current_path).to eq("/shelters/#{shelter_1.id}/pets")
 
-    expect(page).to have_css("img[src*='https://i.pinimg.com/originals/30/f7/eb/30f7eb5efeda4be553a9daa8cb2e90d8.jpg']")
+    expect(page).to have_css("img[src*='#{"https://dogsofsf.com/wp-content/uploads/2016/05/IMG_1391.jpg"}']")
     expect(page).to have_content('Rex')
     expect(page).to have_content("Approximate Age: 8 y/o")
     expect(page).to have_content("Sex: Male")
     expect(page).to_not have_content("Adoptable")
     expect(page).to_not have_content('Hi, I’m Rex. I’m a Basset Hound, I think walks are rather overrated. I prefer to sleep by the fire but I can bribed to get up if there is food. Come hang out by the fire with me.')
+    save_and_open_page
   end
 end
-
-# Then a `POST` request is sent to '/shelters/:shelter_id/pets',
-# a new pet is created for that shelter,
-# that pet has a status of 'adoptable',
